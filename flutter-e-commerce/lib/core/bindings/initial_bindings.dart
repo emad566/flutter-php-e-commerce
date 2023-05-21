208756f195231eedb7fe5296481915190c6b132c
emad566 <emade09@gmail.com>
2023-05-21 23:13:38 +0300

    Initial binding ApiService

---
+++ flutter-e-commerce/lib/core/bindings/initial_bindings.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_e_commerce/core/services/api_services.dart';
+import 'package:get/get.dart';
+
+class InitialBindings extends Bindings{
+  @override
+  void dependencies() {
+    Get.put(ApiService());
+  }
+
+}
--- flutter-e-commerce/lib/core/constants/api_links.dart
+++ flutter-e-commerce/lib/core/constants/api_links.dart
@@ -1,4 +1,4 @@
-abstract class ApiRoutes {
+abstract class ApiLinks {
   static const contentType = 'application/json';
   static const baseURL = 'https://ecommerceapi.marvel-inter.online/';
 }
